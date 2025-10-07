77152c6025d7722f9444d658cc28d99dde355e91
hanuchaudhary <kushchaudharyog@gmail.com>
2025-10-07 09:13:08 +0530

    Revert "Ui"
    This reverts commit 8a0c4cec6fb472fceaf89d17a4c759ee224597a0.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Jost } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -10,7 +10,6 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -40,13 +39,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Navbar />
-            <main className="pt-20 pb-20">{children}</main>
-            <Toaster position="top-center" toastOptions={{
-              style:{
-                borderRadius: '24px',
-                border:"0px"
-              }
-            }} />
+            <main>{children}</main>
+            <Toaster position="top-center" />
           </ThemeProvider>
         </SolanaProvider>
       </body>
--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        hostname: "i.pinimg.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
